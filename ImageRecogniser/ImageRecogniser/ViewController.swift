36f097c770db0ac900c5f2371caaa6dac8aea3da
Jude Molloy <JudeMolloy07@hotmail.com>
2017-11-25 23:39:50 +0000

    Implemented machine learning model and set up image recogniser. Main app works. Work still to be done on the UI.

--- ImageRecogniser/ImageRecogniser/ViewController.swift
+++ ImageRecogniser/ImageRecogniser/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     @IBOutlet weak var recognisedImageView: UIImageView!
     
+    @IBOutlet weak var resultsLabel: UILabel!
+    
     let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
@@ -35,6 +37,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             // Set image on screen to the user selected image
             recognisedImageView.image = userSelectedImage
             
+            // Image is converted to be able to be used by the Vision framework and Core ML
+            guard let ciimage = CIImage(image: userSelectedImage) else {
+                fatalError("Could not convert to CIImage")
+            }
+            
+            // Calls the image to be recognized
+            detect(image: ciimage)
+            
         }
         
         // After image is selected the camera view screen is closed user is returned to recognised image screen
@@ -42,14 +52,56 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         
     }
+    
+    func detect(image: CIImage) {
+        
+        // Creates new instance of the Inceptionv3 model
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML model failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            // Follwing is code that happens when request has been completed
+            
+            // Set results to the results obtained from the recognizer
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image.")
+            }
+            
+            print(results)
+            
+            if let finalResult = results.first {
+                // Set the label depending on results
+                if finalResult.identifier.contains("pizza") {
+                  self.resultsLabel.textColor = #colorLiteral(red: 0.4500938654, green: 0.9813225865, blue: 0.4743030667, alpha: 1)
+                  self.resultsLabel.text = "Pizza!"
+                } else {
+                self.resultsLabel.textColor = #colorLiteral(red: 1, green: 0.4932718873, blue: 0.4739984274, alpha: 1)
+                self.resultsLabel.text = "Not Pizza!"
+            }
+            
+        }
+            
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        // Try to make a request to recognize the image that has been selected
+        do {
+            try handler.perform([request])
+        }
+        catch {
+            print(error)
+        }
+    
 
+    }
+    
     @IBAction func cameraButtonTapped(_ sender: Any) {
         
         // When camera button pressed present camera screen
         present(imagePicker, animated: true, completion: nil)
         
     }
-    
 
 }
-
